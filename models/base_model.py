7e0f602d4809dd20453e74e4c49bc5b89dccac9a
Brent Muha <800@holbertonschool.com>
2019-11-06 21:19:31 +0000

    Update base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,9 @@ Define a BaseModel.
 """
 import uuid
 from datetime import datetime, date, time
-
+from models.engine.file_storage import FileStorage
+# import storage
+from models.__init__ import storage
 
 class BaseModel:
     """Define a BaseModel class."""
@@ -22,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Print the BaseModel attributes."""
@@ -31,6 +34,7 @@ class BaseModel:
     def save(self):
         """Update the attribute 'updated_at' with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary containing keys/values of __dict__."""
@@ -38,5 +42,3 @@ class BaseModel:
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         return self.__dict__
-
-    
