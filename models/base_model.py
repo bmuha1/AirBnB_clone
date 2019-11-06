693d09397fcf7152a04541c7839146378ac3208d
Brent Muha <800@holbertonschool.com>
2019-11-06 19:13:09 +0000

    Add base model

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Define a BaseModel.
+"""
+import uuid
+from datetime import datetime, date, time
+
+
+class BaseModel:
+    """Define a BaseModel class."""
+
+    def __init__(self):
+        """Initialize a BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Print the BaseModel attributes."""
+        return ('[' + str(type(self).__name__) + '] (' + str(self.id) +
+                ') ' + str(self.__dict__))
+
+    def save(self):
+        """Update the attribute 'updated_at' with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing keys/values of __dict__."""
+        self.__dict__.update({'__class__' : str(type(self).__name__)})
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        return self.__dict__
