6bcc1629b18e383037ce8656fcb8dbcbdba96375
Banu Sapakova <banuaksom@gmail.com>
2019-11-06 21:37:42 +0000

    added

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,17 @@ from datetime import datetime, date, time
 class BaseModel:
     """Define a BaseModel class."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if (kwargs):
+            for key, value in kwargs.items():
+                if (key == 'created_at' or key == 'updated_at'):
+                    datetime.strptime(key, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Print the BaseModel attributes."""
@@ -30,3 +36,5 @@ class BaseModel:
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         return self.__dict__
+
+    
