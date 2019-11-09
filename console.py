ed3818a6be14afa82a9ae99879b78f8d14554aaf
Brent Muha <800@holbertonschool.com>
2019-11-09 15:29:47 +0000

    Add create, show, destroy, and all

--- console.py
+++ console.py
@@ -1,13 +1,71 @@
 #!/usr/bin/python3
 """ the entry point of the command interpreter """
-import cmd
+import cmd, sys
 from models.base_model import BaseModel
 import models
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
     prompt = "(hbnb) "
+
+    def do_all(self, line):
+        """Print all instances."""
+        if not line or line == 'BaseModel':
+            print([str(value) for key, value in storage.all().items()])
+        else:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, line):
+        """Delete an instance based on class name and id."""
+        if not line:
+            print('** class name missing **')
+            return
+
+        args = line.split()
+        if args[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            try:
+                key = args[0] + '.' + args[1]
+                del storage.all()[key]
+                storage.save()
+            except:
+                print('** no instance found **')
+
+    def do_show(self, line):
+        """Print an instance based on class name and id."""
+        if not line:
+            print('** class name missing **')
+            return
+
+        args = line.split()
+        if args[0] != 'BaseModel':
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            try:
+                key = args[0] + '.' + args[1]
+                print(storage.all()[key])
+            except KeyError:
+                print('** no instance found **')
+
+    def do_create(self, line):
+        """Create a new instance of BaseModel."""
+        if not line:
+            print('** class name missing **')
+            return
+
+        try:
+            new = eval(line + '()')
+            print(new.id)
+            new.save()
+        except:
+            print("** class doesn't exist **")
 
     def do_quit(self, line):
         """Quit to exit the program"""
@@ -19,8 +77,8 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """If this method is not overridden, repeats the last
-            nonempty command entered"""
+        """Do nothing"""
+        pass
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
